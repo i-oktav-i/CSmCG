98815e8c8bbc790fc108020e7431d5b993446756
Andrey Taranenko <i_okatv_i@mail.ru>
2024-03-17 22:27:05 +0300

    Square strips fix

--- src/pages/lab2/square/fragmentShader.glsl
+++ src/pages/lab2/square/fragmentShader.glsl
@@ -8,9 +8,10 @@ out vec4 color;
 
 void main() {
   int scaledX = int(v_vertextPosition.x * 10.0);
-  bool isRed = (scaledX - (scaledX / 2 * 2)) == 0;
+  
+  bool isOdd = (scaledX - (scaledX / 2 * 2)) == 0;
 
-  if (isRed) {
+  if (isOdd && v_vertextPosition.x >= 0.0 || !isOdd && v_vertextPosition.x < 0.0) {
     color = vec4(1, 0, 0, 1);
   } else {
     color = vec4(0, 1, 0, 1);
